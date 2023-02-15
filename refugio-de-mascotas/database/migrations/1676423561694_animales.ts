43ebb88ebf3b42dc0ec7e1354471aefcd8e2e995
Diego Fernando Bulla Poveda <48418033+dibullap@users.noreply.github.com>
2023-02-14 20:39:11 -0500

    Migraciones, pero con modelo y controlador vacíos

---
+++ refugio-de-mascotas/database/migrations/1676423561694_animales.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'animales'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.integer('codigo_animal').primary();
+      table.string('nombre_animal').notNullable();
+      table.integer('especie').notNullable();
+      table.integer('raza').notNullable();
+      table.binary('sexo').notNullable();
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
